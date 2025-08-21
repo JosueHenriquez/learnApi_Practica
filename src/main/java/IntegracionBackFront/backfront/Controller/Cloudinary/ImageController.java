b6e4679ae0c534beaa1cc75da7e18321b620e59f
Jozhuas <20230102@ricaldone.edu.sv>
2025-08-21 15:09:09 -0600

    termine image

--- src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
+++ src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
@@ -1,10 +1,52 @@
 package IntegracionBackFront.backfront.Controller.Cloudinary;
 
+import IntegracionBackFront.backfront.Services.Cloudinary.CloudinaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/image" +
-        "")
+@RequestMapping("/api/image")
 public class ImageController {
+
+    @Autowired
+    private final CloudinaryService service;
+
+    public ImageController(CloudinaryService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("image")MultipartFile file){
+        try {
+            String imageUrl = service.uploadImage(file);
+            return ResponseEntity.ok(Map.of(
+                    "message", "Imagen subida exitosamente",
+                    "url", imageUrl
+            ));
+        }
+        catch (IOException e){
+            return ResponseEntity.internalServerError().body("Error al subir la imagen");
+        }
+    }
+    @PostMapping("/upload-to-folder")
+    public ResponseEntity<?> uploadImageToFolder(@RequestParam("image") MultipartFile file, @RequestParam String folder){
+        try {
+            String imageUrl = service.uploadImage(file,folder);
+            return ResponseEntity.ok(Map.of(
+                    "message", "Imagen subida exitosamente",
+                    "url", imageUrl
+            ));
+        }
+        catch (IOException e){
+            return ResponseEntity.internalServerError().body("Error al subir la imagen");
+        }
+    }
 }
--- src/main/java/IntegracionBackFront/backfront/Services/Cloudinary/CloudinaryService.java
+++ src/main/java/IntegracionBackFront/backfront/Services/Cloudinary/CloudinaryService.java
@@ -1,11 +1,14 @@
 package IntegracionBackFront.backfront.Services.Cloudinary;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class CloudinaryService {
@@ -21,29 +24,42 @@ public class CloudinaryService {
 
     public String uploadImage(MultipartFile file)throws IOException {
         validateImage(file);
-
+        Map<?, ?> uploadResult = cloudinary.uploader()
+                .upload(file.getBytes(), ObjectUtils.asMap(
+                        "resource_type", "auto",
+                        "quality", "auto:god"
+                ));
+        return (String) uploadResult.get("secure_url");
     }
+    public String uploadImage(MultipartFile file, String folder) throws IOException{
+        validateImage(file);
+        String originalFilename = file.getOriginalFilename();
+        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
+        String uniqueFilename = "img_" + UUID.randomUUID() + fileExtension;
 
+        Map<String, Object> options = ObjectUtils.asMap(
+            "folder", folder,
+            "public_id", uniqueFilename,
+            "use_filename", false,
+            "unique_filename", false,
+            "overwrite", false,
+            "resource_type", "auto",
+            "quality", "auto:good"
+        );
+        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
+        return (String) uploadResult.get("secure_url");
+    }
     private void validateImage(MultipartFile file) {
-        if (file.isEmpty()){
-            throw new IllegalArgumentException("El archivo esta vacio.");
-        }
-        if (file.getSize() > MAX_FILE_SIZE){
-            throw new IllegalArgumentException("El tamaño del archivo no debe ser mayor a 5MB");
-        }
+        if (file.isEmpty()) throw new IllegalArgumentException("El archivo esta vacio.");
+        if (file.getSize() > MAX_FILE_SIZE) throw new IllegalArgumentException("El tamaño del archivo no debe ser mayor a 5MB");
 
         String originalFileName = file.getOriginalFilename();
-        if (originalFileName == null){
-            throw new IllegalArgumentException("Nombre de archivo invalido");
-        }
+        if (originalFileName == null) throw new IllegalArgumentException("Nombre de archivo invalido");
 
         String extension = originalFileName.substring(originalFileName.lastIndexOf(".")).toLowerCase();
-        if (!Arrays.asList(ALLOWED_EXTENSIONS).contains(extension)){
-            throw new IllegalArgumentException("Solo se permite archivos JPG, JPEG, PNG");
-        }
+        if (!Arrays.asList(ALLOWED_EXTENSIONS).contains(extension)) throw new IllegalArgumentException("Solo se permite archivos JPG, JPEG, PNG");
 
-        if (!file.getContentType().startsWith("image/")){
-            throw new IllegalArgumentException("El archivo debe ser una imagen valida")
-        }
+
+        if (!file.getContentType().startsWith("image/")) throw new IllegalArgumentException("El archivo debe ser una imagen valida");
     }
 }
